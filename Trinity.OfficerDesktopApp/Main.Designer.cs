a3962433d2574cd9804a31b95f452fc5b93b8259
Quang Minh <minhdq1982@gmail.com>
2017-12-11 16:54:43 +0800

    Modify Officer Desktop App

--- Trinity.OfficerDesktopApp/Main.Designer.cs
+++ Trinity.OfficerDesktopApp/Main.Designer.cs
@@ -31,28 +31,30 @@
             this.buttonConnect = new System.Windows.Forms.Button();
             this.buttonDisconnect = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.richTextBoxContent = new System.Windows.Forms.RichTextBox();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
             this.buttonSend = new System.Windows.Forms.Button();
             this.labelStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonConnect
             // 
-            this.buttonConnect.Location = new System.Drawing.Point(15, 13);
+            this.buttonConnect.Location = new System.Drawing.Point(20, 16);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonConnect.Name = "buttonConnect";
-            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.Size = new System.Drawing.Size(184, 28);
             this.buttonConnect.TabIndex = 0;
-            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.Text = "Connect to SignalR Server";
             this.buttonConnect.UseVisualStyleBackColor = true;
             this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
             // 
             // buttonDisconnect
             // 
-            this.buttonDisconnect.Location = new System.Drawing.Point(96, 13);
+            this.buttonDisconnect.Location = new System.Drawing.Point(212, 16);
+            this.buttonDisconnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonDisconnect.Name = "buttonDisconnect";
-            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.Size = new System.Drawing.Size(100, 28);
             this.buttonDisconnect.TabIndex = 1;
             this.buttonDisconnect.Text = "Disconnect";
             this.buttonDisconnect.UseVisualStyleBackColor = true;
@@ -60,44 +62,49 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Location = new System.Drawing.Point(16, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.Size = new System.Drawing.Size(55, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Subject";
             // 
-            // textBoxSubject
+            // txtSubject
             // 
-            this.textBoxSubject.Enabled = false;
-            this.textBoxSubject.Location = new System.Drawing.Point(13, 95);
-            this.textBoxSubject.Name = "textBoxSubject";
-            this.textBoxSubject.Size = new System.Drawing.Size(421, 20);
-            this.textBoxSubject.TabIndex = 3;
+            this.txtSubject.Enabled = false;
+            this.txtSubject.Location = new System.Drawing.Point(17, 117);
+            this.txtSubject.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(560, 22);
+            this.txtSubject.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Location = new System.Drawing.Point(16, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.Size = new System.Drawing.Size(57, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Content";
             // 
-            // richTextBoxContent
+            // rtbContent
             // 
-            this.richTextBoxContent.Enabled = false;
-            this.richTextBoxContent.Location = new System.Drawing.Point(13, 134);
-            this.richTextBoxContent.Name = "richTextBoxContent";
-            this.richTextBoxContent.Size = new System.Drawing.Size(421, 177);
-            this.richTextBoxContent.TabIndex = 5;
-            this.richTextBoxContent.Text = "";
+            this.rtbContent.Enabled = false;
+            this.rtbContent.Location = new System.Drawing.Point(17, 165);
+            this.rtbContent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(560, 217);
+            this.rtbContent.TabIndex = 5;
+            this.rtbContent.Text = "";
             // 
             // buttonSend
             // 
             this.buttonSend.Enabled = false;
-            this.buttonSend.Location = new System.Drawing.Point(13, 317);
+            this.buttonSend.Location = new System.Drawing.Point(17, 390);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(421, 23);
+            this.buttonSend.Size = new System.Drawing.Size(561, 28);
             this.buttonSend.TabIndex = 6;
             this.buttonSend.Text = "Send Notification";
             this.buttonSend.UseVisualStyleBackColor = true;
@@ -106,27 +113,29 @@
             // labelStatus
             // 
             this.labelStatus.AutoSize = true;
-            this.labelStatus.Location = new System.Drawing.Point(13, 39);
+            this.labelStatus.Location = new System.Drawing.Point(17, 48);
+            this.labelStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelStatus.Name = "labelStatus";
-            this.labelStatus.Size = new System.Drawing.Size(102, 13);
+            this.labelStatus.Size = new System.Drawing.Size(132, 17);
             this.labelStatus.TabIndex = 7;
             this.labelStatus.Text = "Status: Not connect";
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 352);
+            this.ClientSize = new System.Drawing.Size(595, 433);
             this.Controls.Add(this.labelStatus);
             this.Controls.Add(this.buttonSend);
-            this.Controls.Add(this.richTextBoxContent);
+            this.Controls.Add(this.rtbContent);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.txtSubject);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonDisconnect);
             this.Controls.Add(this.buttonConnect);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
-            this.Text = "Officer Desktop app";
+            this.Text = "Officer Desktop Application";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -138,9 +147,9 @@
         private System.Windows.Forms.Button buttonConnect;
         private System.Windows.Forms.Button buttonDisconnect;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.TextBox txtSubject;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox richTextBoxContent;
+        private System.Windows.Forms.RichTextBox rtbContent;
         private System.Windows.Forms.Button buttonSend;
         private System.Windows.Forms.Label labelStatus;
     }
