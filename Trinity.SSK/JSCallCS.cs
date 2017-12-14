f100768eb476dfb711cff29ee8d4534e362dd1cb
cuongnx <cuongnx88@gmail.com>
2017-12-14 12:58:47 +0700

    add document.cs

--- Trinity.SSK/JSCallCS.cs
+++ Trinity.SSK/JSCallCS.cs
@@ -96,11 +96,13 @@ namespace SSK
                     dalUser.UpdateUser(data.User, data.User.UserId, true);
 
                     dalUserprofile.UpdateUserProfile(data.UserProfile, data.User.UserId, true);
-
+                    //send notifiy to duty officer
+                    APIUtils.SignalR.SendNotificationToDutyOfficer("Supervisee's information changed!", "Please check the Supervisee's information!");
                 }
                 else
                 {
                     dalUserprofile.UpdateUserProfile(data.UserProfile, data.User.UserId, true);
+                    //send notifiy to case officer
                 }
 
                 //send notify to case officer
@@ -113,6 +115,19 @@ namespace SSK
             }
         }
 
+        public void LoadScanDocument()
+        {
+            try
+            {
+                LoadPage("Document.html");
+
+            }
+            catch (Exception)
+            {
+
+                LoadProfile();
+            }
+        }
        
 
         private void actionThread(object pram)
