b883117d7f55a3cf24d3de35c69c28bc834a0e9a
unknown <buixuanquangvinh@gmail.com>
2018-01-02 04:34:42 +0700

    change webcam capture view

--- Trinity.Common/Common/EventAgrs.cs
+++ Trinity.Common/Common/EventAgrs.cs
@@ -115,6 +115,8 @@ namespace Trinity.Common
         public const string GET_LIST_SUPERVISEE_SUCCEEDED = "GetListSuperviseeSucceeded";
         public const string GET_LIST_SUPERVISEE_FAILED = "GetListSuperviseeSFailed";
         public const string OPEN_PICTURE_CAPTURE_FORM = "OpenPictureCaptureForm";
+        public const string CAPTURE_PICTURE = "CapturePicture";
+        public const string CANCEL_CAPTURE_PICTURE = "CancelCapturePicture";
         public const string OPEN_FINGERPRINT_CAPTURE_FORM = "OpenFingerprintCaptureForm";
         public const string PHOTO_CAPTURE_FAILED = "PhotoCaptureFailed";
         public const string FINGERPRINT_CAPTURE_FAILED = "FingerCaptureFailed";
