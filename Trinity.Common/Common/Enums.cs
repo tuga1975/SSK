affdceea78a770169db4865fb3d990e220fbf9ce
cuongnx <cuongnx88@gmail.com>
2018-01-08 20:27:31 +0700

    update setting

--- Trinity.Common/Common/Enums.cs
+++ Trinity.Common/Common/Enums.cs
@@ -232,7 +232,18 @@ public static class EnumStations
 
     public static List<string> GetListStation()
     {
-        return new List<string>() { SSK,SSA,UHP,APS,HSA,ESP};
+        return new List<string>() { SSK, SSA, UHP, APS, HSA, ESP };
     }
 
+}
+
+public enum EnumDayOfWeek
+{
+    Monday = 2,
+    Tuesday = 3,
+    Wednesday = 4,
+    Thursday = 5,
+    Friday = 6,
+    Saturday = 7,
+    Sunday = 8,
 }
