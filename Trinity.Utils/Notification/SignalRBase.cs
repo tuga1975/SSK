e8189b0f1488c5b25db9d46a51f7e74a3142d870
thangxoan91 <thangxoan91@gmail.com>
2018-02-01 17:51:08 +0700

    Add Base Notification

---
+++ Trinity.Utils/Notification/SignalRBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trinity.Utils.Notification
+{
+    public class SignalRBase
+    {
+        private IHubProxy HubProxy { get; set; }
+        const string ServerURI = "http://localhost:8080/signalr";
+        private HubConnection Connection { get; set; }
+    }
+}
