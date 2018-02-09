781aa83848a2b30cf81197b501cfa838fdfacae4
thangxoan91 <thangxoan91@gmail.com>
2018-02-09 15:24:43 +0700

    fix ex

--- Enrolment/Main.cs
+++ Enrolment/Main.cs
@@ -72,16 +72,7 @@ namespace Enrolment
             LayerWeb.Url = new Uri(String.Format("file:///{0}/View/html/Layout.html", CSCallJS.curDir));
             LayerWeb.ObjectForScripting = _jsCallCS;
 
-
-            //ThreadStart newThreadStart = new ThreadStart(newThread_Execute);
-            //Thread newThread = new Thread(newThreadStart);
-            //newThread.Start();
         }
-        //void newThread_Execute()
-        //{
-        //    Thread.Sleep(10000);
-        //    throw new Exception("The method or operation is not implemented.");
-        //}
         //private void Fingerprint_OnFingerprintSucceeded(object sender, CodeBehind.Authentication.FingerprintEventArgs e)
         //{
         //    Session session = Session.Instance;
