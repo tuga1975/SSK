929ee10bc1583a78bf5e95317ce24a6965e6e5c7
Đức Tú <ductu191@live.com>
2018-02-27 08:17:55 +0600

    add publish folder

--- Enrolment/Main.Designer.cs
+++ Enrolment/Main.Designer.cs
@@ -48,7 +48,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(9, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(766, 562);
             this.pictureBox1.TabIndex = 1;
@@ -61,7 +61,6 @@
             this.ClientSize = new System.Drawing.Size(901, 582);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.LayerWeb);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
