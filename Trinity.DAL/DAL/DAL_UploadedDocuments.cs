23a589822e2492c4771d0f775a8176862bed3fd2
Thắng Xoăn <thangxoan91@gmail.com>
2018-03-09 18:15:56 +0700

    ScanDocument from Absence

---
+++ Trinity.DAL/DAL/DAL_UploadedDocuments.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trinity.DAL.Repository;
+
+namespace Trinity.DAL
+{
+    public class DAL_UploadedDocuments
+    {
+        Local_UnitOfWork _localUnitOfWork = new Local_UnitOfWork();
+        Centralized_UnitOfWork _centralizedUnitOfWork = new Centralized_UnitOfWork();
+        
+        public Guid Insert(byte[] content,string UserID)
+        {
+            Trinity.DAL.DBContext.UploadedDocument document = new DBContext.UploadedDocument() {
+                UploadedDate = DateTime.Now,
+                UploadedBy = UserID,
+                Document_ID = Guid.NewGuid(),
+                DocumentContent = content
+            };
+            _localUnitOfWork.GetRepository<Trinity.DAL.DBContext.UploadedDocument>().Add(document);
+            _localUnitOfWork.Save();
+            return document.Document_ID;
+        }
+    }
+}
