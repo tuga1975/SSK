1ade8646ca51193845bccc78065f74d80073b248
ductu191 <ductu191@live.com>
2018-03-14 13:33:18 +0700

    update print and reprint card functions at SSK

--- Trinity.Common/Utils/CommonUtil.cs
+++ Trinity.Common/Utils/CommonUtil.cs
@@ -356,5 +356,35 @@ namespace Trinity.Common
                 return false;
             }
         }
+
+        public static Image ScaleImage(Image image, double ratio)
+        {
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+
+        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
     }
 }
