54a50f7585e83639bd6ec438c12e6c3433085b50
Quang Minh <minhdq1982@gmail.com>
2018-04-12 16:17:19 +0700

    Refactor Trinity.SignalR.Client

--- Trinity.SignalR/Client.cs
+++ Trinity.SignalR/Client.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNet.SignalR.Client;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
 using System.Threading.Tasks;
 using Trinity.Common;
 
@@ -18,7 +15,6 @@ namespace Trinity.SignalR
         private Client()
         {
             ConnectAsync();
-            //Lib.SignalR = this;
         }
         public static Client Instance
         {
@@ -70,7 +66,7 @@ namespace Trinity.SignalR
             Connection.Headers.Add("Station", Station);
             Connection.Closed += ConnectionClosed_Handler;
             HubProxy = Connection.CreateHubProxy("TrinityHub");
-            IncomingEvents();
+            RegisterIncomingEvents();
             ConnectSignalr();
         }
         private void ConnectionClosed_Handler()
@@ -107,35 +103,8 @@ namespace Trinity.SignalR
         public event EventHandler<NotificationInfo> OnBackendAPISend;
         public event EventHandler<NotificationInfo> OnAppointmentBooked;
         public event EventHandler<NotificationInfo> OnAppointmentReported;
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="queue"></param>
-        //public event Action<Trinity.BE.Queue> Event_QueueFinished;
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="deviceId"></param>
-        ///// <param name="deviceStatuses"></param>
-        ///// <param name="Station"></param>
-        //public event Action<int, EnumDeviceStatuses[], string> Event_DeviceStatusUpdate;
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="NotificationID"></param>
-        ///// <param name="UserId"></param>
-        ///// <param name="Subject"></param>
-        ///// <param name="Content"></param>
-        ///// <param name="notificationType"></param>
-        ///// <param name="Station"></param>
-        //public event Action<string, string, string, string, string, string> Event_MessageTo;
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="Station"></param>
-        //public event Action<string> Event_AppDisconnect;
 
-        private void IncomingEvents()
+        private void RegisterIncomingEvents()
         {
             HubProxy.On<NotificationInfo>("OnNewNotification", (notificationInfo) =>
             {
@@ -183,29 +152,7 @@ namespace Trinity.SignalR
                 {
                     OnNewNotification?.Invoke(this, notificationInfo);
                 }
-                //
             });
-            //HubProxy.On<Trinity.BE.Queue>("QueueFinished", (queue) =>
-            //{
-            //    if (Event_QueueFinished != null)
-            //        Event_QueueFinished(queue);
-            //});
-            //HubProxy.On<int, EnumDeviceStatuses[], string>("DeviceStatusUpdate", (deviceId, deviceStatuses, Station) =>
-            //{
-            //    if (Event_DeviceStatusUpdate != null)
-            //        Event_DeviceStatusUpdate(deviceId, deviceStatuses, Station);
-
-            //});
-            //HubProxy.On<string, string, string, string, string, string>("MessageTo", (NotificationID, UserId, Subject, Content, notificationType, Station) =>
-            //{
-            //    if (Event_MessageTo != null)
-            //        Event_MessageTo(NotificationID, UserId, Subject, Content, notificationType, Station);
-            //});
-            //HubProxy.On<string>("AppDisconnect", (Station) =>
-            //{
-            //    if (Event_AppDisconnect != null)
-            //        Event_AppDisconnect(Station);
-            //});
         }
 
         #endregion
@@ -222,32 +169,18 @@ namespace Trinity.SignalR
         public void UserLoggedIn(string userId)
         {
             PostNotification(notificationInfo: new NotificationInfo() { Name = NotificationNames.USER_LOGGED_IN, FromUserId = userId, Type = EnumNotificationTypes.Notification });
-            //await WaitConnectFalse();
-            //if (IsConnected) { }
-            //await HubProxy.Invoke("UserLogined", userId);
         }
+        
         public void UserLoggedOut(string userId)
         {
             PostNotification(notificationInfo: new NotificationInfo() { Name = NotificationNames.USER_LOGGED_OUT, FromUserId = userId, Type = EnumNotificationTypes.Notification });
-            //await WaitConnectFalse();
-            //await HubProxy.Invoke("UserLogout", userID);
         }
 
         public void DeviceStatusChanged(int deviceId, EnumDeviceStatus[] newDeviceStatuses)
         {
             PostNotification(notificationInfo: new NotificationInfo() { Name = NotificationNames.DEVICE_STATUS_CHANGED, Type = EnumNotificationTypes.Notification, Data = new object[] { deviceId, newDeviceStatuses } });
-
-            //await WaitConnectFalse();
-            //try
-            //{
-            //    await HubProxy.Invoke("DeviceStatusUpdate", deviceId, deviceStatuses);
-            //}
-            //catch (Exception)
-            //{
-
-
-            //}
         }
+
         public void SendToDutyOfficer(string fromUserId, string dutyOfficerID, string subject, string content, string notificationType)
         {
             int result = new DAL.DAL_Notification().SendToDutyOfficer(fromUserId, dutyOfficerID, subject, content, notificationType, Station);
@@ -265,36 +198,7 @@ namespace Trinity.SignalR
                 };
                 PostNotification(notificationInfo);
             }
-            //await WaitConnectFalse();
-            //string IDMessage = Guid.NewGuid().ToString().Trim();
-            //if (new DAL.DAL_Notification().SendToDutyOfficer(IDMessage, fromUserId, dutyOfficerID, subject, content, notificationType, Station) > 0)
-            //{
-            //    bool status = await HubProxy.Invoke<bool>("SendToDutyOfficer", IDMessage, fromUserId, dutyOfficerID, subject, content, notificationType);
-            //}
         }
-        //public void SendToAllDutyOfficers(string fromUserId, string subject, string content, string notificationType)
-        //{
-        //    List<BE.Notification> notificationList = new DAL.DAL_Notification().SendToAllDutyOfficers(fromUserId, subject, content, notificationType, Station);
-        //    if (notificationList != null && notificationList.Count > 0)
-        //    {
-        //        string[] dutyOfficers = notificationList.Select(n => n.ToUserId).ToArray();
-        //        NotificationInfo notificationInfo = new NotificationInfo()
-        //        {
-        //            Name = NotificationNames.ALERT_MESSAGE,
-        //            FromUserId = fromUserId,
-        //            ToUserIds = dutyOfficers,
-        //            Content = content,
-        //            Subject = subject,
-        //            Type = notificationType,
-        //            Source = Station
-        //        };
-        //        PostNotification(notificationInfo);
-        //    }
-
-        //    //await WaitConnectFalse();
-        //    //Dictionary<string, string> arrraySend = new DAL.DAL_Notification().SendToAllDutyOfficers(fromUserId, subject, content, notificationType, Station).ToDictionary(f => f.ToUserId, f => f.NotificationID);
-        //    //bool status = await HubProxy.Invoke<bool>("SendToAllDutyOfficers", arrraySend, fromUserId, subject, content, notificationType);
-        //}
 
         public void SendToAppDutyOfficers(string fromUserId, string subject, string content, string notificationType, string station = null, bool isInsertDB = true)
         {
@@ -342,11 +246,7 @@ namespace Trinity.SignalR
         public void QueueCompleted(string userId)
         {
             PostNotification(notificationInfo: new NotificationInfo() { Name = NotificationNames.QUEUE_COMPLETED, Type = EnumNotificationTypes.Notification, FromUserId = userId });
-            //await WaitConnectFalse();
-            //Trinity.BE.Queue queue = new DAL.DAL_QueueNumber().GetMyQueueToday(UserId);
-            //await HubProxy.Invoke("QueueFinished", queue);
         }
-
 
         public void BackendAPISend(string NotificationNames, object Data)
         {
@@ -375,7 +275,7 @@ namespace Trinity.SignalR
         {
             PostNotification(notificationInfo: new NotificationInfo() { Name = NotificationNames.SSA_COMPLETED, UserID = UserId });
         }
-        public void SSAPrintingLabel(string UserId)
+        public void SSALabelPrinted(string UserId)
         {
             PostNotification(notificationInfo: new NotificationInfo() { Name = NotificationNames.SSA_PRINTING_LABEL, UserID = UserId });
         }
