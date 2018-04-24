2b08de09c20cff1ffdd12f34518121995678821a
Quang Minh <minhdq1982@gmail.com>
2018-04-24 13:56:36 +0700

    Change queue screen name

--- Trinity.ARK/FormQueueNumber.Designer.cs
+++ Trinity.ARK/FormQueueNumber.Designer.cs
@@ -33,14 +33,14 @@
             // 
             // FormQueueNumber
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 308);
+            this.ClientSize = new System.Drawing.Size(885, 379);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormQueueNumber";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.Text = "Administration and Registration Kiosk";
+            this.Text = "Admin & Register Kiosk";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
